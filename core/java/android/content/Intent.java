3ec586333d3496834e1f48bf047e80fac50989e6
liquid0624 <rjbirnesser@gmail.com>
2013-12-28 00:31:17 -0500

    [2/2] Frameworks: HALO
    Credit: drcmda (paranoid android)
    
    PS1: Initial port
         Fix conficts with notification LED
         Fix derp
         Major derp
         HALO Mods
         Rebase
         Fix blackist, code clean and added some TODOs
    PS2: Fix build
    PS3: Fix bubble sometimes doesn't respond on new notification
    PS4: Align halo icon
    
    Change-Id: I3e79945433d61da090ca263f4e2989be9987f11d

--- core/java/android/content/Intent.java
+++ core/java/android/content/Intent.java
@@ -3610,6 +3610,14 @@ public class Intent implements Parcelable, Cloneable {
      * saw.   This can only be used in conjunction with {@link #FLAG_ACTIVITY_NEW_TASK}.
      */
     public static final int FLAG_ACTIVITY_TASK_ON_HOME = 0X00004000;
+
+     /*
+     *
+     * If set, this intent will always match start up as a floating window
+     * in mutil window scenarios.
+     */
+    public static final int FLAG_FLOATING_WINDOW = 0x00002000;
+
     /**
      * If set in an Intent passed to {@link Context#startActivity Context.startActivity()},
      * this flag will cause a newly launching task to be resized according to the split
