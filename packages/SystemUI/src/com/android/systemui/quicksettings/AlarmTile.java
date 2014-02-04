4f1a84a1f8ea9658e8bf266efbb1767d383dcee4
Robert Burns <burnsra@gmail.com>
2014-02-04 09:21:38 -0500

    Modify alarm tile to show alarms when clicked
    Change-Id: I76002b37dfe81c4e8973b3ef3f70fd67c26b1e4e

--- packages/SystemUI/src/com/android/systemui/quicksettings/AlarmTile.java
+++ packages/SystemUI/src/com/android/systemui/quicksettings/AlarmTile.java
@@ -43,7 +43,7 @@ public class AlarmTile extends QuickSettingsTile {
         mOnClick = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startSettingsActivity(new Intent(AlarmClock.ACTION_SET_ALARM));
+                startSettingsActivity(new Intent(AlarmClock.ACTION_SHOW_ALARMS));
             }
         };
 
