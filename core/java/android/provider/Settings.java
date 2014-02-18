ec57b296661a89ec178aa0f41f53f4ee5311def9
Chris Longo <goldflame09@gmail.com>
2014-02-18 13:49:05 -0500

    Add evening time to header
    Change-Id: I5d9a41d92803d64f67cf2fb9dd2c4294a947efc3

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4422,12 +4422,6 @@ public final class Settings {
         public static final String RECENT_PANEL_FAVORITES = "recent_panel_favorites";
 
         /**
-         * Whether to use the custom status bar header or not
-         * @hide
-         */
-        public static final String STATUS_BAR_CUSTOM_HEADER = "status_bar_custom_header";
-
-        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
