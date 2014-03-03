3a0d94e33692f679399e0f43b8567b91806c9d9b
liquid0624 <rjbirnesser@gmail.com>
2014-03-03 01:59:44 -0500

    Remove derp from location manager service

--- services/java/com/android/server/LocationManagerService.java
+++ services/java/com/android/server/LocationManagerService.java
@@ -1079,7 +1079,7 @@ public class LocationManagerService extends ILocationManager.Stub {
     public List<String> getProviders(Criteria criteria, boolean enabledOnly) {
         int allowedResolutionLevel = getCallerAllowedResolutionLevel();
         ArrayList<String> out;
-        int uid = Binder.getCallingUid();;
+        int uid = Binder.getCallingUid();
         long identity = Binder.clearCallingIdentity();
         try {
             synchronized (mLock) {
@@ -1475,7 +1475,7 @@ public class LocationManagerService extends ILocationManager.Stub {
                         packageName, workSource, hideFromAppOps);
                 if (receiver != null) {
                         requestLocationUpdatesLocked(sanitizedRequest, receiver, pid,
-                                                     uid, packageName);
+                            uid, packageName);
                 }
             }
         } finally {
