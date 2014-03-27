CyanogenMod/android_frameworks_base

Base: On-The-Go: Toggle flashlight when shaking
Change-Id: I42c75ef17640ea6485ee57240abee8582036d60d

core/java/com/android/internal/util/nameless/listeners/ShakeDetector.java
@@ -1,0 +1,241 @@
+// Copyright 2010 Square, Inc.
+package com.android.internal.util.nameless.listeners;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Detects phone shaking. If > 75% of the samples taken in the past 0.5s are
+ * accelerating, the device is a) shaking, or b) free falling 1.84m (h =
+ * 1/2*g*t^2*3/4).
+ *
+ * @author Bob Lee (bob@squareup.com)
+ * @author Eric Burke (eric@squareup.com)
+ */
+public class ShakeDetector implements SensorEventListener {
+
+    /**
+     * When the magnitude of total acceleration exceeds this
+     * value, the phone is accelerating.
+     */
+    private static final int ACCELERATION_THRESHOLD = 13;
+
+    /** Listens for shakes. */
+    public interface Listener {
+        /** Called on the main thread when the device is shaken. */
+        void hearShake();
+    }
+
+    private final SampleQueue queue = new SampleQueue();
+    private final Listener listener;
+
+    private SensorManager sensorManager;
+    private Sensor        accelerometer;
+
+    public ShakeDetector(Listener listener) {
+        this.listener = listener;
+    }
+
+    /**
+     * Starts listening for shakes on devices with appropriate hardware.
+     *
+     * @returns true if the device supports shake detection.
+     */
+    public boolean start(SensorManager sensorManager) {
+        if (accelerometer != null) {
+            return true;
+        }
+
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        // If this phone has an accelerometer, listen to it.
+        if (accelerometer != null) {
+            this.sensorManager = sensorManager;
+            sensorManager.registerListener(this, accelerometer,
+                    SensorManager.SENSOR_DELAY_FASTEST);
+        }
+        return accelerometer != null;
+    }
+
+    /**
+     * Stops listening.  Safe to call when already stopped.  Ignored on devices
+     * without appropriate hardware.
+     */
+    public void stop() {
+        if (accelerometer != null) {
+            sensorManager.unregisterListener(this, accelerometer);
+            sensorManager = null;
+            accelerometer = null;
+        }
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        boolean accelerating = isAccelerating(event);
+        long timestamp = event.timestamp;
+        queue.add(timestamp, accelerating);
+        if (queue.isShaking()) {
+            queue.clear();
+            listener.hearShake();
+        }
+    }
+
+    /** Returns true if the device is currently accelerating. */
+    private boolean isAccelerating(SensorEvent event) {
+        float ax = event.values[0];
+        float ay = event.values[1];
+        float az = event.values[2];
+
+        final double magnitude = Math.sqrt(ax * ax + ay * ay + az * az);
+        return magnitude > ACCELERATION_THRESHOLD;
+    }
+
+    /** Queue of samples. Keeps a running average. */
+    static class SampleQueue {
+
+        /** Window size in ns. Used to compute the average. */
+        private static final long MAX_WINDOW_SIZE = 500000000; // 0.5s
+        private static final long MIN_WINDOW_SIZE = MAX_WINDOW_SIZE >> 1; // 0.25s
+
+        /**
+         * Ensure the queue size never falls below this size, even if the device
+         * fails to deliver this many events during the time window. The LG Ally
+         * is one such device.
+         */
+        private static final int MIN_QUEUE_SIZE = 4;
+
+        private final SamplePool pool = new SamplePool();
+
+        private Sample oldest;
+        private Sample newest;
+        private int    sampleCount;
+        private int    acceleratingCount;
+
+        /**
+         * Adds a sample.
+         *
+         * @param timestamp    in nanoseconds of sample
+         * @param accelerating true if > {@link #ACCELERATION_THRESHOLD}.
+         */
+        void add(long timestamp, boolean accelerating) {
+            // Purge samples that proceed window.
+            purge(timestamp - MAX_WINDOW_SIZE);
+
+            // Add the sample to the queue.
+            Sample added = pool.acquire();
+            added.timestamp = timestamp;
+            added.accelerating = accelerating;
+            added.next = null;
+            if (newest != null) {
+                newest.next = added;
+            }
+            newest = added;
+            if (oldest == null) {
+                oldest = added;
+            }
+
+            // Update running average.
+            sampleCount++;
+            if (accelerating) {
+                acceleratingCount++;
+            }
+        }
+
+        /** Removes all samples from this queue. */
+        void clear() {
+            while (oldest != null) {
+                Sample removed = oldest;
+                oldest = removed.next;
+                pool.release(removed);
+            }
+            newest = null;
+            sampleCount = 0;
+            acceleratingCount = 0;
+        }
+
+        /** Purges samples with timestamps older than cutoff. */
+        void purge(long cutoff) {
+            while (sampleCount >= MIN_QUEUE_SIZE
+                    && oldest != null && cutoff - oldest.timestamp > 0) {
+                // Remove sample.
+                Sample removed = oldest;
+                if (removed.accelerating) {
+                    acceleratingCount--;
+                }
+                sampleCount--;
+
+                oldest = removed.next;
+                if (oldest == null) {
+                    newest = null;
+                }
+                pool.release(removed);
+            }
+        }
+
+        /** Copies the samples into a list, with the oldest entry at index 0. */
+        List<Sample> asList() {
+            List<Sample> list = new ArrayList<Sample>();
+            Sample s = oldest;
+            while (s != null) {
+                list.add(s);
+                s = s.next;
+            }
+            return list;
+        }
+
+        /**
+         * Returns true if we have enough samples and more than 3/4 of those samples
+         * are accelerating.
+         */
+        boolean isShaking() {
+            return newest != null
+                    && oldest != null
+                    && newest.timestamp - oldest.timestamp >= MIN_WINDOW_SIZE
+                    && acceleratingCount >= (sampleCount >> 1) + (sampleCount >> 2);
+        }
+    }
+
+    /** An accelerometer sample. */
+    static class Sample {
+        /** Time sample was taken. */
+        long timestamp;
+
+        /** If acceleration > {@link #ACCELERATION_THRESHOLD}. */
+        boolean accelerating;
+
+        /** Next sample in the queue or pool. */
+        Sample next;
+    }
+
+    /** Pools samples. Avoids garbage collection. */
+    static class SamplePool {
+        private Sample head;
+
+        /** Acquires a sample from the pool. */
+        Sample acquire() {
+            Sample acquired = head;
+            if (acquired == null) {
+                acquired = new Sample();
+            } else {
+                // Remove instance from pool.
+                head = acquired.next;
+            }
+            return acquired;
+        }
+
+        /** Returns a sample to the pool. */
+        void release(Sample sample) {
+            sample.next = head;
+            head = sample;
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    }
+}

packages/SystemUI/src/com/android/systemui/nameless/onthego/OnTheGoService.java
@@ -33,8 +33,10 @@ import android.content.res.Resources;
 import android.graphics.PixelFormat;
 import android.graphics.SurfaceTexture;
 import android.hardware.Camera;
+import android.hardware.SensorManager;
 import android.os.Handler;
 import android.os.IBinder;
+import android.os.UserHandle;
 import android.provider.Settings;
 import android.util.Log;
 import android.view.TextureView;
@@ -43,11 +45,13 @@ import android.view.WindowManager;
 import android.widget.FrameLayout;
 
 import com.android.internal.util.nameless.NamelessUtils;
+import com.android.internal.util.nameless.constants.FlashLightConstants;
+import com.android.internal.util.nameless.listeners.ShakeDetector;
 import com.android.systemui.R;
 
 import java.io.IOException;
 
-public class OnTheGoService extends Service {
+public class OnTheGoService extends Service implements ShakeDetector.Listener {
 
     private static final String  TAG   = "OnTheGoService";
     private static final boolean DEBUG = false;
@@ -74,6 +78,8 @@ public class OnTheGoService extends Service {
     private FrameLayout         mOverlay;
     private Camera              mCamera;
     private NotificationManager mNotificationManager;
+    private SensorManager       mSensorManager;
+    private ShakeDetector       mShakeDetector;
 
     @Override
     public IBinder onBind(Intent intent) {
@@ -92,6 +98,10 @@ public class OnTheGoService extends Service {
         registerReceiver(mAlphaReceiver, alphaFilter);
         final IntentFilter cameraFilter = new IntentFilter(ACTION_TOGGLE_CAMERA);
         registerReceiver(mCameraReceiver, cameraFilter);
+
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mShakeDetector = new ShakeDetector(this);
+        mShakeDetector.start(mSensorManager);
     }
 
     private void unregisterReceivers() {
@@ -101,6 +111,12 @@ public class OnTheGoService extends Service {
         try {
             unregisterReceiver(mCameraReceiver);
         } catch (Exception ignored) { }
+
+        if (mShakeDetector != null) {
+            mShakeDetector.stop();
+            mShakeDetector = null;
+            mSensorManager = null;
+        }
     }
 
     private final BroadcastReceiver mAlphaReceiver = new BroadcastReceiver() {
@@ -389,6 +405,27 @@ public class OnTheGoService extends Service {
     private void logDebug(String msg) {
         if (DEBUG) {
             Log.e(TAG, msg);
+        }
+    }
+
+    private final        Object  mShakeLock     = new Object();
+    private final static int     SHAKE_TIMEOUT  = 1000;
+    private              boolean mIsShakeLocked = false;
+
+    @Override
+    public void hearShake() {
+        synchronized (mShakeLock) {
+            if (!mIsShakeLocked) {
+                final Intent intent = new Intent(FlashLightConstants.ACTION_TOGGLE_STATE);
+                sendBroadcastAsUser(intent, UserHandle.CURRENT);
+                mIsShakeLocked = true;
+                mHandler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        mIsShakeLocked = false;
+                    }
+                }, SHAKE_TIMEOUT);
+            }
         }
     }
 }

zeetherocker/frameworks_base

Conflicts:
	packages/SystemUI/src/com/android/systemui/nameless/onthego/OnTheGoService.java

core/java/com/android/internal/util/ose/listeners/ShakeDetector.java
@@ -1,0 +1,241 @@
+// Copyright 2010 Square, Inc.
+package com.android.internal.util.ose.listeners;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Detects phone shaking. If > 75% of the samples taken in the past 0.5s are
+ * accelerating, the device is a) shaking, or b) free falling 1.84m (h =
+ * 1/2*g*t^2*3/4).
+ *
+ * @author Bob Lee (bob@squareup.com)
+ * @author Eric Burke (eric@squareup.com)
+ */
+public class ShakeDetector implements SensorEventListener {
+
+    /**
+     * When the magnitude of total acceleration exceeds this
+     * value, the phone is accelerating.
+     */
+    private static final int ACCELERATION_THRESHOLD = 13;
+
+    /** Listens for shakes. */
+    public interface Listener {
+        /** Called on the main thread when the device is shaken. */
+        void hearShake();
+    }
+
+    private final SampleQueue queue = new SampleQueue();
+    private final Listener listener;
+
+    private SensorManager sensorManager;
+    private Sensor        accelerometer;
+
+    public ShakeDetector(Listener listener) {
+        this.listener = listener;
+    }
+
+    /**
+     * Starts listening for shakes on devices with appropriate hardware.
+     *
+     * @returns true if the device supports shake detection.
+     */
+    public boolean start(SensorManager sensorManager) {
+        if (accelerometer != null) {
+            return true;
+        }
+
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        // If this phone has an accelerometer, listen to it.
+        if (accelerometer != null) {
+            this.sensorManager = sensorManager;
+            sensorManager.registerListener(this, accelerometer,
+                    SensorManager.SENSOR_DELAY_FASTEST);
+        }
+        return accelerometer != null;
+    }
+
+    /**
+     * Stops listening.  Safe to call when already stopped.  Ignored on devices
+     * without appropriate hardware.
+     */
+    public void stop() {
+        if (accelerometer != null) {
+            sensorManager.unregisterListener(this, accelerometer);
+            sensorManager = null;
+            accelerometer = null;
+        }
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        boolean accelerating = isAccelerating(event);
+        long timestamp = event.timestamp;
+        queue.add(timestamp, accelerating);
+        if (queue.isShaking()) {
+            queue.clear();
+            listener.hearShake();
+        }
+    }
+
+    /** Returns true if the device is currently accelerating. */
+    private boolean isAccelerating(SensorEvent event) {
+        float ax = event.values[0];
+        float ay = event.values[1];
+        float az = event.values[2];
+
+        final double magnitude = Math.sqrt(ax * ax + ay * ay + az * az);
+        return magnitude > ACCELERATION_THRESHOLD;
+    }
+
+    /** Queue of samples. Keeps a running average. */
+    static class SampleQueue {
+
+        /** Window size in ns. Used to compute the average. */
+        private static final long MAX_WINDOW_SIZE = 500000000; // 0.5s
+        private static final long MIN_WINDOW_SIZE = MAX_WINDOW_SIZE >> 1; // 0.25s
+
+        /**
+         * Ensure the queue size never falls below this size, even if the device
+         * fails to deliver this many events during the time window. The LG Ally
+         * is one such device.
+         */
+        private static final int MIN_QUEUE_SIZE = 4;
+
+        private final SamplePool pool = new SamplePool();
+
+        private Sample oldest;
+        private Sample newest;
+        private int    sampleCount;
+        private int    acceleratingCount;
+
+        /**
+         * Adds a sample.
+         *
+         * @param timestamp    in nanoseconds of sample
+         * @param accelerating true if > {@link #ACCELERATION_THRESHOLD}.
+         */
+        void add(long timestamp, boolean accelerating) {
+            // Purge samples that proceed window.
+            purge(timestamp - MAX_WINDOW_SIZE);
+
+            // Add the sample to the queue.
+            Sample added = pool.acquire();
+            added.timestamp = timestamp;
+            added.accelerating = accelerating;
+            added.next = null;
+            if (newest != null) {
+                newest.next = added;
+            }
+            newest = added;
+            if (oldest == null) {
+                oldest = added;
+            }
+
+            // Update running average.
+            sampleCount++;
+            if (accelerating) {
+                acceleratingCount++;
+            }
+        }
+
+        /** Removes all samples from this queue. */
+        void clear() {
+            while (oldest != null) {
+                Sample removed = oldest;
+                oldest = removed.next;
+                pool.release(removed);
+            }
+            newest = null;
+            sampleCount = 0;
+            acceleratingCount = 0;
+        }
+
+        /** Purges samples with timestamps older than cutoff. */
+        void purge(long cutoff) {
+            while (sampleCount >= MIN_QUEUE_SIZE
+                    && oldest != null && cutoff - oldest.timestamp > 0) {
+                // Remove sample.
+                Sample removed = oldest;
+                if (removed.accelerating) {
+                    acceleratingCount--;
+                }
+                sampleCount--;
+
+                oldest = removed.next;
+                if (oldest == null) {
+                    newest = null;
+                }
+                pool.release(removed);
+            }
+        }
+
+        /** Copies the samples into a list, with the oldest entry at index 0. */
+        List<Sample> asList() {
+            List<Sample> list = new ArrayList<Sample>();
+            Sample s = oldest;
+            while (s != null) {
+                list.add(s);
+                s = s.next;
+            }
+            return list;
+        }
+
+        /**
+         * Returns true if we have enough samples and more than 3/4 of those samples
+         * are accelerating.
+         */
+        boolean isShaking() {
+            return newest != null
+                    && oldest != null
+                    && newest.timestamp - oldest.timestamp >= MIN_WINDOW_SIZE
+                    && acceleratingCount >= (sampleCount >> 1) + (sampleCount >> 2);
+        }
+    }
+
+    /** An accelerometer sample. */
+    static class Sample {
+        /** Time sample was taken. */
+        long timestamp;
+
+        /** If acceleration > {@link #ACCELERATION_THRESHOLD}. */
+        boolean accelerating;
+
+        /** Next sample in the queue or pool. */
+        Sample next;
+    }
+
+    /** Pools samples. Avoids garbage collection. */
+    static class SamplePool {
+        private Sample head;
+
+        /** Acquires a sample from the pool. */
+        Sample acquire() {
+            Sample acquired = head;
+            if (acquired == null) {
+                acquired = new Sample();
+            } else {
+                // Remove instance from pool.
+                head = acquired.next;
+            }
+            return acquired;
+        }
+
+        /** Returns a sample to the pool. */
+        void release(Sample sample) {
+            sample.next = head;
+            head = sample;
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    }
+}

packages/SystemUI/src/com/android/systemui/ose/onthego/OnTheGoService.java
@@ -1,18 +1,21 @@
 /*
- * Copyright (C) 2014 The OSE Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+* <!--
+*    Copyright (C) 2014 The OSE Project
+*
+*    This program is free software: you can redistribute it and/or modify
+*    it under the terms of the GNU General Public License as published by
+*    the Free Software Foundation, either version 3 of the License, or
+*    (at your option) any later version.
+*
+*    This program is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU General Public License for more details.
+*
+*    You should have received a copy of the GNU General Public License
+*    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+* -->
+*/
 
 package com.android.systemui.ose.onthego;
 
@@ -30,8 +33,10 @@ import android.content.res.Resources;
 import android.graphics.PixelFormat;
 import android.graphics.SurfaceTexture;
 import android.hardware.Camera;
+import android.hardware.SensorManager;
 import android.os.Handler;
 import android.os.IBinder;
+import android.os.UserHandle;
 import android.provider.Settings;
 import android.util.Log;
 import android.view.TextureView;
@@ -40,11 +45,13 @@ import android.view.WindowManager;
 import android.widget.FrameLayout;
 
 import com.android.internal.util.ose.NamelessUtils;
+import com.android.internal.util.ose.constants.FlashLightConstants;
+import com.android.internal.util.ose.listeners.ShakeDetector;
 import com.android.systemui.R;
 
 import java.io.IOException;
 
-public class OnTheGoService extends Service {
+public class OnTheGoService extends Service implements ShakeDetector.Listener {
 
     private static final String  TAG   = "OnTheGoService";
     private static final boolean DEBUG = false;
@@ -71,6 +78,8 @@ public class OnTheGoService extends Service {
     private FrameLayout         mOverlay;
     private Camera              mCamera;
     private NotificationManager mNotificationManager;
+    private SensorManager       mSensorManager;
+    private ShakeDetector       mShakeDetector;
 
     @Override
     public IBinder onBind(Intent intent) {
@@ -89,6 +98,10 @@ public class OnTheGoService extends Service {
         registerReceiver(mAlphaReceiver, alphaFilter);
         final IntentFilter cameraFilter = new IntentFilter(ACTION_TOGGLE_CAMERA);
         registerReceiver(mCameraReceiver, cameraFilter);
+
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mShakeDetector = new ShakeDetector(this);
+        mShakeDetector.start(mSensorManager);
     }
 
     private void unregisterReceivers() {
@@ -98,6 +111,12 @@ public class OnTheGoService extends Service {
         try {
             unregisterReceiver(mCameraReceiver);
         } catch (Exception ignored) { }
+
+        if (mShakeDetector != null) {
+            mShakeDetector.stop();
+            mShakeDetector = null;
+            mSensorManager = null;
+        }
     }
 
     private final BroadcastReceiver mAlphaReceiver = new BroadcastReceiver() {
@@ -388,5 +407,25 @@ public class OnTheGoService extends Service {
             Log.e(TAG, msg);
         }
     }
-}
 
+    private final        Object  mShakeLock     = new Object();
+    private final static int     SHAKE_TIMEOUT  = 1000;
+    private              boolean mIsShakeLocked = false;
+
+    @Override
+    public void hearShake() {
+        synchronized (mShakeLock) {
+            if (!mIsShakeLocked) {
+                final Intent intent = new Intent(FlashLightConstants.ACTION_TOGGLE_STATE);
+                sendBroadcastAsUser(intent, UserHandle.CURRENT);
+                mIsShakeLocked = true;
+                mHandler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        mIsShakeLocked = false;
+                    }
+                }, SHAKE_TIMEOUT);
+            }
+        }
+    }
+}