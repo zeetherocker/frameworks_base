DirtyUnicorns/android_frameworks_base

Squash in the latest from Nameless ROM
- http://gerrit.nameless-rom.org/#/c/3593/
- http://gerrit.nameless-rom.org/#/c/3594/
- http://gerrit.nameless-rom.org/#/c/3595/
- http://gerrit.nameless-rom.org/#/c/3724/
- http://gerrit.nameless-rom.org/#/c/3733/5

Change-Id: I1d9ec26063b3cd71fe12e01aeb918b7a2b4eea75

packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsController.java
@@ -53,6 +53,7 @@ import static com.android.internal.util.slim.QSConstants.TILE_REBOOT;
 import static com.android.internal.util.slim.QSConstants.TILE_NETWORKADB;
 import static com.android.internal.util.slim.QSConstants.TILE_GPS;
 import static com.android.internal.util.slim.QSConstants.TILE_FCHARGE;
+import static com.android.internal.util.slim.QSConstants.TILE_ONTHEGO;
 
 import android.content.BroadcastReceiver;
 import android.content.ContentResolver;
@@ -105,6 +106,7 @@ import com.android.systemui.quicksettings.WifiAPTile;
 import com.android.systemui.quicksettings.RebootTile;
 import com.android.systemui.quicksettings.GpsTile;
 import com.android.systemui.quicksettings.FastChargeTile;
+import com.android.systemui.quicksettings.OnTheGoTile;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -252,6 +254,8 @@ public class QuickSettingsController {
                 qs = new GpsTile(mContext, this);
             } else if (tile.contains(TILE_FCHARGE)) {
                 qs = new FastChargeTile(mContext, this);
+            } else if (tile.contains(TILE_ONTHEGO)) {
+                qs = new OnTheGoTile(mContext, this);
             } else if (tile.equals(TILE_THEME)) {
                 qs = new ThemeTile(mContext, this);
             }

policy/src/com/android/internal/policy/impl/GlobalActions.java
@@ -72,6 +72,8 @@ import android.widget.ImageView.ScaleType;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.android.internal.util.nameless.NamelessActions;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
@@ -477,7 +479,8 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
                         R.string.global_action_onthego) {
 
                         public void onPress() {
-                            startOnTheGo();
+                            NamelessActions.processAction(mContext,
+                                    NamelessActions.ACTION_ONTHEGO_TOGGLE);
                         }
 
                         public boolean onLongPress() {

zeetherocker/frameworks_base

Conflicts:
	core/java/android/provider/Settings.java
	core/java/com/android/internal/util/ose/QSConstants.java
	packages/SystemUI/res/values/custom_strings.xml
	packages/SystemUI/res/values/strings.xml
	packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsController.java

packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsController.java
@@ -56,6 +56,7 @@ import static com.android.internal.util.ose.QSConstants.TILE_REBOOT;
 import static com.android.internal.util.ose.QSConstants.TILE_WEATHER;
 import static com.android.internal.util.ose.QSConstants.TILE_INTERNALMEMORY;
 import static com.android.internal.util.ose.QSConstants.TILE_FCHARGE;
+import static com.android.internal.util.ose.QSConstants.TILE_ONTHEGO;
 
 import android.app.Activity;
 import android.app.ActivityManagerNative;
@@ -120,6 +121,7 @@ import com.android.systemui.quicksettings.WifiAPTile;
 import com.android.systemui.quicksettings.RebootTile;
 import com.android.systemui.quicksettings.Weather;
 import com.android.systemui.quicksettings.FastChargeTile;
+import com.android.systemui.quicksettings.OnTheGoTile;
 import com.android.systemui.R;
 
 import java.io.File;
@@ -279,6 +281,8 @@ public class QuickSettingsController {
                 qs = new InternalMemory(mContext, this);
             } else if (tile.contains(TILE_FCHARGE)) {
                 qs = new FastChargeTile(mContext, this);
+            } else if (tile.contains(TILE_ONTHEGO)) {
+                qs = new OnTheGoTile(mContext, this);
             }
 
             if (qs != null) {

policy/src/com/android/internal/policy/impl/GlobalActions.java
@@ -74,6 +74,8 @@ import android.widget.ImageView.ScaleType;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.android.internal.util.ose.NamelessActions;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
@@ -248,7 +250,8 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
                         R.string.global_action_onthego) {
 
                         public void onPress() {
-                            startOnTheGo();
+                            NamelessActions.processAction(mContext,
+                                    NamelessActions.ACTION_ONTHEGO_TOGGLE);
                         }
 
                         public boolean onLongPress() {