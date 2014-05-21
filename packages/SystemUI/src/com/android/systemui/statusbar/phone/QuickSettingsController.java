CyanogenMod/android_frameworks_base

QuickSettings : Add BatterySaver Tile (1/2)

packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsController.java
@@ -58,6 +58,7 @@ import static com.android.internal.util.slim.QSConstants.TILE_PROFILE;
 import static com.android.internal.util.slim.QSConstants.TILE_CAMERA;
 import static com.android.internal.util.slim.QSConstants.TILE_INTERNALMEMORY;
 import static com.android.internal.util.slim.QSConstants.TILE_ONTHEGO;
+import static com.android.internal.util.slim.QSConstants.TILE_BATTERYSAVER;
 
 import android.content.BroadcastReceiver;
 import android.content.ContentResolver;
@@ -115,6 +116,7 @@ import com.android.systemui.quicksettings.FChargeTile;
 import com.android.systemui.quicksettings.ProfileTile;
 import com.android.systemui.quicksettings.CameraTile;
 import com.android.systemui.quicksettings.OnTheGoTile;
+import com.android.systemui.quicksettings.BatterySaverTile;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -272,6 +274,8 @@ public class QuickSettingsController {
                 qs = new InternalMemory(mContext, this);
             } else if (tile.equals(TILE_ONTHEGO)) {
                 qs = new OnTheGoTile(mContext, this);
+            } else if (tile.equals(TILE_BATTERYSAVER)) {
+                qs = new BatterySaverTile(mContext, this);
             } else if (tile.equals(TILE_PROFILE)) {
                 mTileStatusUris.add(Settings.System.getUriFor(Settings.System.SYSTEM_PROFILES_ENABLED));
                 if (DeviceUtils.systemProfilesEnabled(resolver)) {

zeetherocker/frameworks_base

Conflicts:
	core/java/com/android/internal/util/liquid/QSConstants.java
	packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsController.java
	core/java/com/android/internal/util/ose/QSConstants.java

packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsController.java
@@ -58,11 +58,13 @@ import static com.android.internal.util.ose.QSConstants.TILE_INTERNALMEMORY;
 import static com.android.internal.util.ose.QSConstants.TILE_FCHARGE;
 import static com.android.internal.util.ose.QSConstants.TILE_ONTHEGO;
 import static com.android.internal.util.ose.QSConstants.TILE_SHAKE;
+import static com.android.internal.util.ose.QSConstants.TILE_BATTERYSAVER;
 
 import android.app.Activity;
 import android.app.ActivityManagerNative;
 import android.app.AlertDialog;
 import android.app.Dialog;
+
 import android.content.BroadcastReceiver;
 import android.content.ContentResolver;
 import android.content.Context;
@@ -89,6 +91,7 @@ import com.android.systemui.quicksettings.AirplaneModeTile;
 import com.android.systemui.quicksettings.AlarmTile;
 import com.android.systemui.quicksettings.AutoRotateTile;
 import com.android.systemui.quicksettings.BatteryTile;
+import com.android.systemui.quicksettings.BatterySaverTile;
 import com.android.systemui.quicksettings.BluetoothTile;
 import com.android.systemui.quicksettings.BrightnessTile;
 import com.android.systemui.quicksettings.BugReportTile;
@@ -288,6 +291,8 @@ public class QuickSettingsController {
                 qs = new FastChargeTile(mContext, this);
             } else if (tile.contains(TILE_ONTHEGO)) {
                 qs = new OnTheGoTile(mContext, this);
+            } else if (tile.equals(TILE_BATTERYSAVER)) {
+                qs = new BatterySaverTile(mContext, this);
             }
 
             if (qs != null) {