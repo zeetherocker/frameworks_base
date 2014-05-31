d130df0893f110c7fd5f530e3acd7c8f966a532c
Schubi <schubi@erlangen.ccc.de>
2014-05-31 13:09:36 +0200

    Slim Recents: Option to show topmost task [1/2]
    Change-Id: Ib39d9b87b0fd30b4d34d4292af39e891038f337a
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4648,6 +4648,13 @@ public final class Settings {
         public static final String RECENT_PANEL_EXPANDED_MODE = "recent_panel_expanded_mode";
 
         /**
+         * Recent panel: Show topmost task
+         *
+         * @hide
+         */
+        public static final String RECENT_PANEL_SHOW_TOPMOST = "recent_panel_show_topmost";
+
+        /**
          * Either if notification peek is enabled of disabled
          * 0 = disabled (default)
          * 1 = enabled
@@ -4714,7 +4721,6 @@ public final class Settings {
         public static final String HOVER_EXCLUDE_LOW_PRIORITY = "hover_exclude_low_priority";
 
         /**
-         * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
          * NOTE: Settings are backed up and restored in the order they appear
