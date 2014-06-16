3612dd98f68f16ed21d893472be306e26c60ada6
Schubi <schubi@erlangen.ccc.de>
2014-06-16 22:06:55 +0200

    HOVER: Option to exclude topmost app [1/2]
    Originally taken from Tigger2014:
    https://github.com/Tigger2014/android_frameworks_base-1/commit/5af0e1c84e86e9b4e6947163aaf58e9ab541079d
    
    Change-Id: I6da9027af4c7bc74945a06b97b876f94cf817844
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4711,6 +4711,13 @@ public final class Settings {
          * @hide
          */
         public static final String HOVER_EXCLUDE_LOW_PRIORITY = "hover_exclude_low_priority";
+		
+        /**
+         * Hover: Option to exclude topmost app, default is 1 (on).
+         *
+         * @hide
+         */
+        public static final String HOVER_EXCLUDE_TOPMOST = "hover_exclude_topmost";
 
         /**
          * keys and easy to update.
