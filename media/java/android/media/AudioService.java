26465ecd70cbab92cb976ed32f8582d08d93359c
Lars Greiss <kufikugel@googlemail.com>
2014-07-06 23:47:17 +0200

    AudioManager: make swap volume buttons RTL aware
    The layout of the volume panel is already RTL aware...the problem is
    if it is in landscape and the buttons are swapped they go into opposite
    direction then the volume panel.
    
    So this swap should only happen on LTR languages and RTL should be left
    like it is.
    
    So both LTR and RTL have it logical.
    
    Change-Id: Ie5fbe3085b40ad35814f53ed19736d7800dad78e

--- media/java/android/media/AudioService.java
+++ media/java/android/media/AudioService.java
@@ -508,7 +508,7 @@ public class AudioService extends IAudioService.Stub {
         sSoundEffectVolumeDb = context.getResources().getInteger(
                 com.android.internal.R.integer.config_soundEffectVolumeDb);
 
-        mVolumePanel = new VolumePanel(context, this);
+        mVolumePanel = new VolumePanel(context, null, this); // first created panel
 
         mTelephonyManager = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
 
