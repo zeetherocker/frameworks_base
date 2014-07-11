CyanogenMod/android_frameworks_base

fb: Change density on the fly
Change-Id: I7ee16d32ee882fea22367f49f69f4da744f3aa45

Conflicts:
	core/java/android/view/WindowManagerPolicy.java
	policy/src/com/android/internal/policy/impl/PhoneWindowManager.java

core/java/com/android/internal/util/slim/DensityUtils.java
@@ -1,0 +1,67 @@
+package com.android.internal.util.slim;
+
+import android.os.SystemProperties;
+
+import java.io.FileInputStream;
+import java.util.Properties;
+
+public class DensityUtils {
+
+    public static final String DENSITY_PERSIST_PROP = "persist.sys.lcd_density";
+    public static final String DENSITY_SYSTEM_PROP = "ro.sf.lcd_density";
+
+    private static final int DENSITY_DEFAULT = 160;
+
+    private static int mCurrentDensity;
+    private static int mMaxDensity = getSystemDensity();
+
+    public static void setCurrentDensity(int density) {
+        mCurrentDensity = density;
+    }
+
+    public static int getCurrentDensity() {
+        if (mCurrentDensity == 0) {
+            return getDensityFromProp();
+        }
+        return mCurrentDensity;
+    }
+
+    public static int getDensityFromProp() {
+        return SystemProperties.getInt(DENSITY_PERSIST_PROP,
+                SystemProperties.getInt(DENSITY_SYSTEM_PROP, DENSITY_DEFAULT));
+    }
+
+    public static int getSystemDensity() {
+        return SystemProperties.getInt(DENSITY_SYSTEM_PROP, getDensityFromProp());
+    }
+
+    public static int getSlimDefaultDensity() {
+        Properties prop = new Properties();
+        FileInputStream fis = null;
+        try {
+            fis = new FileInputStream("/system/build.prop");
+            prop.load(fis);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (fis != null) {
+                    fis.close();
+                }
+            } catch (Exception e) {
+            }
+        }
+        return Integer.parseInt(prop.getProperty(DENSITY_PERSIST_PROP, "-1"));
+    }
+
+    public static int getMinimumDensity() {
+        int min = -1;
+        int[] densities = { 91, 121, 161, 241, 321, 481 };
+        for (int density : densities) {
+            if (density < mMaxDensity) {
+                min = density;
+            }
+        }
+        return min;
+    }
+}

zeetherocker/frameworks_base

	core/java/android/content/res/Resources.java
	core/java/android/util/DisplayMetrics.java
	core/java/android/view/IWindowManager.aidl
	graphics/java/android/graphics/Bitmap.java
	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
	services/java/com/android/server/wm/WindowManagerService.java

core/java/com/android/internal/util/ose/DensityUtils.java
@@ -1,0 +1,67 @@
+package com.android.internal.util.ose;
+
+import android.os.SystemProperties;
+
+import java.io.FileInputStream;
+import java.util.Properties;
+
+public class DensityUtils {
+
+    public static final String DENSITY_PERSIST_PROP = "persist.sys.lcd_density";
+    public static final String DENSITY_SYSTEM_PROP = "ro.sf.lcd_density";
+
+    private static final int DENSITY_DEFAULT = 160;
+
+    private static int mCurrentDensity;
+    private static int mMaxDensity = getSystemDensity();
+
+    public static void setCurrentDensity(int density) {
+        mCurrentDensity = density;
+    }
+
+    public static int getCurrentDensity() {
+        if (mCurrentDensity == 0) {
+            return getDensityFromProp();
+        }
+        return mCurrentDensity;
+    }
+
+    public static int getDensityFromProp() {
+        return SystemProperties.getInt(DENSITY_PERSIST_PROP,
+                SystemProperties.getInt(DENSITY_SYSTEM_PROP, DENSITY_DEFAULT));
+    }
+
+    public static int getSystemDensity() {
+        return SystemProperties.getInt(DENSITY_SYSTEM_PROP, getDensityFromProp());
+    }
+
+    public static int getSlimDefaultDensity() {
+        Properties prop = new Properties();
+        FileInputStream fis = null;
+        try {
+            fis = new FileInputStream("/system/build.prop");
+            prop.load(fis);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (fis != null) {
+                    fis.close();
+                }
+            } catch (Exception e) {
+            }
+        }
+        return Integer.parseInt(prop.getProperty(DENSITY_PERSIST_PROP, "-1"));
+    }
+
+    public static int getMinimumDensity() {
+        int min = -1;
+        int[] densities = { 91, 121, 161, 241, 321, 481 };
+        for (int density : densities) {
+            if (density < mMaxDensity) {
+                min = density;
+            }
+        }
+        return min;
+    }
+}