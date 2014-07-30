CyanogenMod/android_frameworks_base

Filter spam notifications
Change-Id: Ibf2377cd7491c7e0e9e9c9657e9e65eef1876ee3

packages/SystemUI/src/com/android/systemui/cm/SpamOpenHelper.java
@@ -1,0 +1,44 @@
+package com.android.systemui.cm;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import static com.android.internal.util.cm.SpamFilter.SpamContract.*;
+
+public class SpamOpenHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "spam.db";
+    private static final int VERSION = 4;
+    private static final String CREATE_PACKAGES_TABLE =
+            "create table " + PackageTable.TABLE_NAME + "(" +
+             PackageTable.ID + " INTEGER PRIMARY KEY," +
+             PackageTable.PACKAGE_NAME + " TEXT UNIQUE);";
+    private static final String CREATE_NOTIFICATIONS_TABLE =
+            "create table " + NotificationTable.TABLE_NAME + "(" +
+             NotificationTable.ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+             NotificationTable.PACKAGE_ID + " INTEGER," +
+             NotificationTable.MESSAGE_TEXT + " STRING," +
+             NotificationTable.LAST_BLOCKED + " INTEGER," +
+             NotificationTable.NORMALIZED_TEXT + " STRING," +
+             NotificationTable.COUNT + " INTEGER DEFAULT 0);";
+
+    private Context mContext;
+
+    public SpamOpenHelper(Context context) {
+        super(context, DATABASE_NAME, null, VERSION);
+        mContext = context;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_PACKAGES_TABLE);
+        db.execSQL(CREATE_NOTIFICATIONS_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        mContext.deleteDatabase(DATABASE_NAME);
+        onCreate(db);
+    }
+
+}

packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -25,6 +25,7 @@ import android.app.admin.DevicePolicyManager;
 import android.content.BroadcastReceiver;
 import android.content.ComponentName;
 import android.content.ContentResolver;
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
@@ -73,13 +74,18 @@ import android.widget.TextView;
 import com.android.internal.statusbar.IStatusBarService;
 import com.android.internal.statusbar.StatusBarIcon;
 import com.android.internal.statusbar.StatusBarIconList;
+import com.android.internal.util.cm.SpamFilter;
 import com.android.internal.widget.SizeAdaptiveLayout;
 import com.android.systemui.R;
 import com.android.systemui.RecentsComponent;
 import com.android.systemui.SearchPanelView;
 import com.android.systemui.SystemUI;
+import com.android.systemui.cm.SpamMessageProvider;
+import com.android.systemui.statusbar.NotificationData.Entry;
 import com.android.systemui.statusbar.phone.KeyguardTouchDelegate;
 import com.android.systemui.statusbar.policy.NotificationRowLayout;
+
+import static com.android.internal.util.cm.SpamFilter.SpamContract.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -145,6 +151,14 @@ public abstract class BaseStatusBar extends SystemUI implements
     protected int mRowHeight;
 
     protected FrameLayout mStatusBarContainer;
+    private final static Uri mSpamMessageUri;
+    static {
+        Uri.Builder builder = new Uri.Builder();
+        builder.scheme(ContentResolver.SCHEME_CONTENT);
+        builder.authority(SpamMessageProvider.AUTHORITY);
+        builder.appendPath("message");
+        mSpamMessageUri = builder.build();
+    }
 
     private Runnable mPanelCollapseRunnable = new Runnable() {
         @Override
@@ -473,7 +487,10 @@ public abstract class BaseStatusBar extends SystemUI implements
         return new View.OnLongClickListener() {
             @Override
             public boolean onLongClick(View v) {
-                final String packageNameF = (String) v.getTag();
+                final NotificationData.Entry entry = (Entry) v.getTag();
+                StatusBarNotification sbNotification = entry.notification;
+                final Notification notification = sbNotification.getNotification();
+                final String packageNameF = sbNotification.getPackageName();
                 if (packageNameF == null) return false;
                 if (v.getWindowToken() == null) return false;
                 mNotificationBlamePopup = new PopupMenu(mContext, v);
@@ -521,6 +538,13 @@ public abstract class BaseStatusBar extends SystemUI implements
                                     .getSystemService(Context.ACTIVITY_SERVICE);
                             am.clearApplicationUserData(packageNameF,
                                     new FakeClearUserDataObserver());
+                        } else if (item.getItemId() == R.id.notification_spam_item) {
+                            ContentValues values = new ContentValues();
+                            String message = SpamFilter.getNotificationContent(notification);
+                            values.put(NotificationTable.MESSAGE_TEXT, message);
+                            values.put(PackageTable.PACKAGE_NAME, packageNameF);
+                            mContext.getContentResolver().insert(mSpamMessageUri, values);
+                            removeNotification(entry.key);
                         } else {
                             return false;
                         }
@@ -759,7 +783,7 @@ public abstract class BaseStatusBar extends SystemUI implements
                 R.layout.status_bar_notification_row, parent, false);
 
         // for blaming (see SwipeHelper.setLongPressListener)
-        row.setTag(sbn.getPackageName());
+        row.setTag(entry);
 
         workAroundBadLayerDrawableOpacity(row);
         View vetoButton = updateNotificationVetoButton(row, sbn);

zeetherocker/frameworks_base

Conflicts:
	packages/SystemUI/AndroidManifest.xml
	packages/SystemUI/res/values/cm_strings.xml
	packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
	services/java/com/android/server/NotificationManagerService.java

packages/SystemUI/src/com/android/systemui/ose/SpamOpenHelper.java
@@ -1,0 +1,46 @@
+package com.android.systemui.ose;
+
+import com.android.internal.util.ose.SpamFilter.SpamContract.NotificationTable;
+import com.android.internal.util.ose.SpamFilter.SpamContract.PackageTable;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class SpamOpenHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "spam.db";
+    private static final int VERSION = 4;
+    private static final String CREATE_PACKAGES_TABLE =
+            "create table " + PackageTable.TABLE_NAME + "(" +
+             PackageTable.ID + " INTEGER PRIMARY KEY," +
+             PackageTable.PACKAGE_NAME + " TEXT UNIQUE);";
+    private static final String CREATE_NOTIFICATIONS_TABLE =
+            "create table " + NotificationTable.TABLE_NAME + "(" +
+             NotificationTable.ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+             NotificationTable.PACKAGE_ID + " INTEGER," +
+             NotificationTable.MESSAGE_TEXT + " STRING," +
+             NotificationTable.LAST_BLOCKED + " INTEGER," +
+             NotificationTable.NORMALIZED_TEXT + " STRING," +
+             NotificationTable.COUNT + " INTEGER DEFAULT 0);";
+
+    private Context mContext;
+
+    public SpamOpenHelper(Context context) {
+        super(context, DATABASE_NAME, null, VERSION);
+        mContext = context;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_PACKAGES_TABLE);
+        db.execSQL(CREATE_NOTIFICATIONS_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        mContext.deleteDatabase(DATABASE_NAME);
+        onCreate(db);
+    }
+
+}

packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -41,9 +41,6 @@ import android.app.Notification;
 import android.app.PendingIntent;
 import android.app.TaskStackBuilder;
 import android.app.admin.DevicePolicyManager;
-import android.content.BroadcastReceiver;
-import android.content.ActivityNotFoundException;
-import android.content.ComponentName;
 import android.content.*;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.IPackageDataObserver;
@@ -110,6 +107,9 @@ import com.android.internal.statusbar.IStatusBarService;
 import com.android.internal.statusbar.StatusBarIcon;
 import com.android.internal.statusbar.StatusBarIconList;
 import com.android.systemui.statusbar.phone.Ticker;
+import com.android.internal.util.ose.SpamFilter;
+import com.android.internal.util.ose.SpamFilter.SpamContract.NotificationTable;
+import com.android.internal.util.ose.SpamFilter.SpamContract.PackageTable;
 import com.android.internal.widget.SizeAdaptiveLayout;
 import com.android.internal.util.ose.ButtonConfig;
 import com.android.internal.util.ose.DeviceUtils;
@@ -127,6 +127,8 @@ import com.android.systemui.statusbar.notification.Hover;
 import com.android.systemui.statusbar.notification.HoverCling;
 import com.android.systemui.statusbar.notification.NotificationHelper;
 import com.android.systemui.statusbar.notification.Peek;
+import com.android.systemui.ose.SpamMessageProvider;
+import com.android.systemui.statusbar.NotificationData.Entry;
 import com.android.systemui.statusbar.phone.KeyguardTouchDelegate;
 import com.android.systemui.statusbar.phone.NavigationBarOverlay;
 import com.android.systemui.statusbar.halo.Halo;
@@ -168,6 +170,12 @@ public abstract class BaseStatusBar extends SystemUI implements
 
     public static final int EXPANDED_LEAVE_ALONE = -10000;
     public static final int EXPANDED_FULL_OPEN = -10001;
+
+    private static final Uri SPAM_MESSAGE_URI = new Uri.Builder()
+            .scheme(ContentResolver.SCHEME_CONTENT)
+            .authority(SpamMessageProvider.AUTHORITY)
+            .appendPath("message")
+            .build();
 
     protected CommandQueue mCommandQueue;
     protected INotificationManager mNotificationManager;
@@ -890,7 +898,9 @@ public abstract class BaseStatusBar extends SystemUI implements
         return new View.OnLongClickListener() {
             @Override
             public boolean onLongClick(View v) {
-                final String packageNameF = (String) v.getTag();
+                final NotificationData.Entry entry = (Entry) v.getTag();
+                final StatusBarNotification sbNotification = entry.notification;
+                final String packageNameF = sbNotification.getPackageName();
                 if (packageNameF == null) return false;
                 if (v.getWindowToken() == null) return false;
                 mNotificationBlamePopup = new PopupMenu(mContext, v);
@@ -938,6 +948,14 @@ public abstract class BaseStatusBar extends SystemUI implements
                                     .getSystemService(Context.ACTIVITY_SERVICE);
                             am.clearApplicationUserData(packageNameF,
                                     new FakeClearUserDataObserver());
+                        } else if (item.getItemId() == R.id.notification_spam_item) {
+                            ContentValues values = new ContentValues();
+                            String message = SpamFilter.getNotificationContent(
+                                    sbNotification.getNotification());
+                            values.put(NotificationTable.MESSAGE_TEXT, message);
+                            values.put(PackageTable.PACKAGE_NAME, packageNameF);
+                            mContext.getContentResolver().insert(SPAM_MESSAGE_URI, values);
+                            removeNotification(entry.key);
                         } else {
                             return false;
                         }
@@ -1266,7 +1284,7 @@ public abstract class BaseStatusBar extends SystemUI implements
                 R.layout.status_bar_notification_row, parent, false);
 
         // for blaming (see SwipeHelper.setLongPressListener)
-        row.setTag(sbn.getPackageName());
+        row.setTag(entry);
 
         workAroundBadLayerDrawableOpacity(row);
         View vetoButton = updateNotificationVetoButton(row, sbn);